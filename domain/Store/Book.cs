802d1ce10905c168c0b81ace3cb363738ba76421
danil555555 <maslovdanil45228@gmail.com>
2024-11-02 05:01:01 +0300

    Создать веб приложение

--- domain/Store/Book.cs
+++ domain/Store/Book.cs
@@ -1,6 +1,15 @@
-﻿namespace Store;
-
-public class Class1
+﻿namespace Store
 {
 
+    public class Book
+    {
+        public string Title { get; }
+        public int Id { get; }
+
+        public Book(int id, string title)
+        {
+            Title = title;
+            Id = id;
+        }
+    }
 }
---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using.System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art of Programming"),
+            new Book(2, "Refactoing"),
+            new Book(3, "C Programming Language")
+        };
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart))
+                .ToArray();
+
+        }
+    }
+}
