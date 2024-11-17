e029bd49f9e0449025ca8286b65018bffdd54974
danil555555 <maslovdanil45228@gmail.com>
2024-11-17 06:26:04 +0300

    Добавить книгу в корзину #5

--- presentation/Store.Web/Program.cs
+++ presentation/Store.Web/Program.cs
@@ -4,9 +4,19 @@ using Store.Memory;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(20);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddSingleton<BookService>();
 builder.Services.AddSingleton<IBookRepository, BookRepository>();
-builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -24,6 +34,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
